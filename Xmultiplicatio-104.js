9d9e09459cebdbc5aec59b0d5980f6c519fbfbdd
Estarossa09 <kararawi79@gmail.com>
2023-01-13 17:52:19 +0300

    Multiplication

---
+++ Xmultiplicatio-104.js
@@ -0,0 +1,36 @@
+
+const num1 = Math.ceil(Math.random() * 20);
+const num2 = Math.ceil(Math.random() * 20);
+
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+  score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`
+
+questionEl.innerText = `What is ${num1} * ${num2} ?`
+
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage()
+  } else {
+    score--;
+    updateLocalStorage()
+  }
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score))
+}
